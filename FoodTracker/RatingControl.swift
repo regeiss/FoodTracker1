4adcc3318da4f47cd696173e7597cab08975297a
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2018-07-01 22:27:39 -0300

    Classe controle

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Roberto Edgar Geiss on 01/07/2018.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView
+{
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
