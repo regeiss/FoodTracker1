d19755492dc63eded877a5498c27538f37df2cc6
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2018-07-04 23:17:21 -0300

    Incluido Tableview e imagens

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Roberto Edgar Geiss on 04/07/2018.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell
+{
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
